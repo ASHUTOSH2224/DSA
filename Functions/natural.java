bf9c236a911ebedeb1afa26663308084f95aa1e4
AnugyaBajpai2454 <98964990+ASHUTOSH2224@users.noreply.github.com>
2024-08-20 23:49:27 +0530

    Initial commit

---
+++ Functions/natural.java
@@ -0,0 +1,10 @@
+import java.io.*;
+public class natural {
+    static int findSum(int n) {
+        return n * (n + 1) / 2;
+    }
+    public static void main(String args[]) {
+        int n = 5;
+        System.out.println(findSum(n));
+    }
+}
